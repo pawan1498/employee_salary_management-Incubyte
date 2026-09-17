a72771b645c11cb46e1b276d3bf4c9b8154abc2b
Pawan Sharma <pawan.sharma@w3villa.com>
2026-09-17 21:04:18 +0530

    test: initialise RSpec for TDD
    Switch from Minitest to RSpec so upcoming employee and salary behaviour can be specified outside-in.

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,9 @@ module EmployeeSalaryManagement
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false
+    end
   end
 end
